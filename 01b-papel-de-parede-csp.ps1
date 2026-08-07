cb61616018f00c55e7a7854a7ba01cc1bb29533c
Nathan Gomes <nathancarvalho1322@gmail.com>
2026-08-07 10:24:45 -0400

    Change wallpaper download URL from JPEG to PNG

--- 01b-papel-de-parede-csp.ps1
+++ 01b-papel-de-parede-csp.ps1
@@ -3,7 +3,7 @@ $dir = 'C:\Program Files\MFTelecom'
 $img = "$dir\wallpaper.png"
 
 New-Item -ItemType Directory -Path $dir -Force | Out-Null
-Invoke-WebRequest 'https://raw.githubusercontent.com/NathanGomes1322/Ad_mftelecon/main/wallpaper.jpeg' -OutFile $img -UseBasicParsing
+Invoke-WebRequest 'https://raw.githubusercontent.com/NathanGomes1322/Ad_mftelecon/main/wallpaper.png' -OutFile $img -UseBasicParsing
 
 $k = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\PersonalizationCSP'
 New-Item -Path $k -Force | Out-Null
