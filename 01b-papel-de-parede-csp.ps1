e58e788f0e4ef3f7103c662e054fd6f8091c54d5
Nathan Gomes <nathancarvalho1322@gmail.com>
2026-08-07 10:23:04 -0400

    Create 01b-papel-de-parede-csp.ps1

---
+++ 01b-papel-de-parede-csp.ps1
@@ -0,0 +1,14 @@
+$ErrorActionPreference = 'Stop'
+$dir = 'C:\Program Files\MFTelecom'
+$img = "$dir\wallpaper.png"
+
+New-Item -ItemType Directory -Path $dir -Force | Out-Null
+Invoke-WebRequest 'https://raw.githubusercontent.com/NathanGomes1322/Ad_mftelecon/main/wallpaper.jpeg' -OutFile $img -UseBasicParsing
+
+$k = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\PersonalizationCSP'
+New-Item -Path $k -Force | Out-Null
+New-ItemProperty -Path $k -Name DesktopImagePath   -Value $img -PropertyType String -Force | Out-Null
+New-ItemProperty -Path $k -Name DesktopImageUrl    -Value $img -PropertyType String -Force | Out-Null
+New-ItemProperty -Path $k -Name DesktopImageStatus -Value 1    -PropertyType DWord  -Force | Out-Null
+
+Write-Output "OK - $env:COMPUTERNAME - reiniciar para aplicar"
